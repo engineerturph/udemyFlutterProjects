c5f7af08ede9be8b79f2338156bf22de7b4395ed
engineerturph <engineerturph@gmail.com>
2022-06-17 14:57:50 +0300

    xylophone done

--- xylophone_flutter/lib/main.dart
+++ xylophone_flutter/lib/main.dart
@@ -1,16 +1,50 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
 
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+  void playSound(int type) async {
+    final player = AudioPlayer();
+    await player.play(AssetSource('note$type.wav'));
+  }
+
+  Expanded buildKey({int soundNum: 1, Color color: Colors.blue}) {
+    return Expanded(
+      child: TextButton(
+        style: ButtonStyle(
+          backgroundColor: MaterialStateProperty.all<Color>(color),
+        ),
+        onPressed: () async {
+          playSound(soundNum);
+        },
+        child: Container(),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
-          child: Container(),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              buildKey(soundNum: 1, color: Colors.blue),
+              buildKey(soundNum: 2, color: Colors.red),
+              buildKey(soundNum: 3, color: Colors.yellow),
+              buildKey(soundNum: 4, color: Colors.grey),
+              buildKey(soundNum: 5, color: Colors.green),
+              buildKey(soundNum: 6, color: Colors.greenAccent),
+              buildKey(soundNum: 7, color: Colors.teal),
+            ],
+          ),
         ),
       ),
     );
   }
 }
+
+//flutter run --no-sound-null-safety
