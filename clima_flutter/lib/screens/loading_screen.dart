7a39f24614e40a7a36c6caaefe4931619f87c974
engineerturph <engineerturph@gmail.com>
2022-06-24 18:40:24 +0300

    clima progress

--- clima_flutter/lib/screens/loading_screen.dart
+++ clima_flutter/lib/screens/loading_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:clima_flutter/services/location.dart';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -6,17 +7,38 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+  //Sadece ilk kuruldugunda calisir
+  void initState() async {
+    super.initState();
+    Location location = Location();
+    await location.getCurLocation();
+  }
+
   @override
   Widget build(BuildContext context) {
+    String myMargin = 'abc';
+    double? myMarginAsADouble;
+    try {
+      myMarginAsADouble = double.parse(myMargin);
+    } catch (e) {
+      print(e);
+    }
+    //widget her yeniden uretildiginde calisir
     return Scaffold(
-      body: Center(
-        child: RaisedButton(
-          onPressed: () {
-            //Get the current location
-          },
+      body: Container(
+        margin: EdgeInsets.all(myMarginAsADouble ?? 30),
+        color: Colors.red,
+        child: ElevatedButton(
+          onPressed: () {},
           child: Text('Get Location'),
         ),
       ),
     );
   }
 }
+
+//Sadece state yok edildiginde calisir
+// void deactivate() {
+//   super.deactivate();
+//   print('deactivate called');
+// }
