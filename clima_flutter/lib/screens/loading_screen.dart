147480b8a780c1e027729ebb8dd0a7a92e4f3b21
engineerturph <engineerturph@gmail.com>
2022-06-25 17:57:06 +0300

    clima progress

--- clima_flutter/lib/screens/loading_screen.dart
+++ clima_flutter/lib/screens/loading_screen.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:clima_flutter/services/location.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+const apiKey = '348095a05a97f48edcfb11be35556758';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -8,10 +12,32 @@ class LoadingScreen extends StatefulWidget {
 
 class _LoadingScreenState extends State<LoadingScreen> {
   //Sadece ilk kuruldugunda calisir
+  double? latitude;
+  double? longitude;
+
   void initState() async {
     super.initState();
+    getLocation();
+  }
+
+  void getLocation() async {
     Location location = Location();
     await location.getCurLocation();
+  }
+
+  void getData() async {
+    http.Response response = await http.get(
+        'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid=$apiKey');
+
+    if (response.statusCode == 200) {
+      String data = response.body;
+      var decodedData = jsonDecode(data);
+      var longitude =
+          decodedData['coord']['lon']; //boyle jsonu kendinde decode edebilirsin
+
+    } else {
+      print(response.statusCode);
+    }
   }
 
   @override
